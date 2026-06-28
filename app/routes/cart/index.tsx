3b4cf9f3648822464107d818fad64db88bcefbcf
jagoblast <blastjago@gmail.com>
2026-06-29 01:30:55 +0700

    Update index.tsx

--- app/routes/cart/index.tsx
+++ app/routes/cart/index.tsx
@@ -1,10 +1,16 @@
 import { createRoute } from 'honox/factory'
+import { getCookie } from 'hono/cookie'
 import CartView from '../../islands/CartView'
 
 export default createRoute((c) => {
+  // 1. Cek sesi langsung dari server
+  const isLoggedIn = !!getCookie(c, 'customer_session')
+
   return c.render(
-    // Kita panggil Island komponen interaktif keranjang
-    <CartView />,
+    <div className="w-full">
+      {/* 2. Lempar status login ke Island Frontend */}
+      <CartView isLoggedIn={isLoggedIn} />
+    </div>,
     { title: 'Keranjang Belanja' }
   )
 })
