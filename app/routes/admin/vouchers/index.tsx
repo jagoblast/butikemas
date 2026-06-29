9eb796d0b6eb536ab83a91b578da2b7e85516050
jagoblast <blastjago@gmail.com>
2026-06-29 12:34:31 +0700

    Create index.tsx

---
+++ app/routes/admin/vouchers/index.tsx
@@ -0,0 +1,9 @@
+import AdminVoucherList from '../../../islands/AdminVoucherList'
+
+export default function AdminVouchersPage() {
+  return (
+    <div className="p-6">
+      <AdminVoucherList />
+    </div>
+  )
+}
