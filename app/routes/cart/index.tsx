ac5fd3b33cb95998c3c17dad5563fa116ea8f688
jagoblast <blastjago@gmail.com>
2026-06-29 10:03:50 +0700

    Update index.tsx

--- app/routes/cart/index.tsx
+++ app/routes/cart/index.tsx
@@ -3,8 +3,8 @@ import { getCookie } from 'hono/cookie'
 import CartView from '../../islands/CartView'
 
 export default createRoute((c) => {
-  // Cek otentikasi di server
-  const isLoggedIn = !!getCookie(c, 'customer_session')
+  // PERBAIKAN: Gunakan nama cookie yang BARU agar tombol checkout tidak melempar ke halaman login
+  const isLoggedIn = !!getCookie(c, 'butik_cust_session')
 
   return c.render(
     <CartView isLoggedIn={isLoggedIn} />,
