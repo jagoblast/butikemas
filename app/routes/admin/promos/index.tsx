4fcfe244d9e12e69cb6d0bd6f1fba1fc46835d62
jagoblast <blastjago@gmail.com>
2026-06-29 12:52:45 +0700

    Create index.tsx

---
+++ app/routes/admin/promos/index.tsx
@@ -0,0 +1,9 @@
+import AdminPromoList from '../../../islands/AdminPromoList'
+
+export default function AdminPromosPage() {
+  return (
+    <div className="p-6">
+      <AdminPromoList />
+    </div>
+  )
+}
